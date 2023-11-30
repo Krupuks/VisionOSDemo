985c0a75b59978a6168c7ab3c412bce4e6029ce7
Kevin Van Rooy <kevinvanrooy96@hotmail.com>
2023-11-30 14:24:08 +0100

    Initial Commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by Kevin Van Rooy on 30/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
